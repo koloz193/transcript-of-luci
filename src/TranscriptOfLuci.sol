f91b907a8fed4ef868b06bc158ab788957924d61
Zach Kolodny <zach.kolodny@gmail.com>
2024-11-26 11:56:00 -0500

    optimize

--- src/TranscriptOfLuci.sol
+++ src/TranscriptOfLuci.sol
@@ -4,19 +4,11 @@ pragma solidity 0.8.28;
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract TranscriptOfLuci is AccessControlUpgradeable, OwnableUpgradeable {
-    bytes32 public constant COMMENTER_ROLE =
-        bytes32(uint256(keccak256("COMMENTER_ROLE")) - 1);
+/*//////////////////////////////////////////////////////////////////////////
+                                  Structs
+//////////////////////////////////////////////////////////////////////////*/
 
-    mapping(uint256 x => mapping(uint256 y => Comment[] comments))
-        private comments;
-
-    mapping(address nftContract => mapping(uint256 tokenId => Comment comment))
-        private commentByNft;
-
-    // Comment[] private rawComments;
-
-    struct Coordinate {
+struct Coordinate {
         uint256 x;
         uint256 y;
     }
@@ -27,16 +19,52 @@ contract TranscriptOfLuci is AccessControlUpgradeable, OwnableUpgradeable {
         Coordinate coordinate;
     }
 
+contract TranscriptOfLuci is AccessControlUpgradeable, OwnableUpgradeable {
+    /*//////////////////////////////////////////////////////////////////////////
+                                  Constants
+    //////////////////////////////////////////////////////////////////////////*/
+
+    bytes32 public constant COMMENTER_ROLE =
+        bytes32(uint256(keccak256("COMMENTER_ROLE")) - 1);
+
+    /*//////////////////////////////////////////////////////////////////////////
+                            Private Storage Variables
+    //////////////////////////////////////////////////////////////////////////*/
+
+    mapping(uint256 x => mapping(uint256 y => uint256[] commentIndexes))
+        private comments;
+
+    mapping(address nftContract => mapping(uint256 tokenId => uint256 commentIndex))
+        private commentByNft;
+
+    Comment[] private rawComments;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                  Constructor
+    //////////////////////////////////////////////////////////////////////////*/
+
     constructor(bool _disableInitializer) {
         if (_disableInitializer) {
             _disableInitializers();
         }
     }
 
+    /*//////////////////////////////////////////////////////////////////////////
+                                  Initializer
+    //////////////////////////////////////////////////////////////////////////*/
+
     function initialize(address _owner) external initializer {
         _grantRole(DEFAULT_ADMIN_ROLE, _owner);
         _transferOwnership(_owner);
+
+        Comment memory empty;
+
+        rawComments.push(empty);
     }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                            Add Comments Functions
+    //////////////////////////////////////////////////////////////////////////*/
 
     function addComment(
         uint256 _x,
@@ -59,7 +87,9 @@ contract TranscriptOfLuci is AccessControlUpgradeable, OwnableUpgradeable {
         uint256 _x,
         uint256 _y,
         address _commenter,
-        string calldata _comment
+        string calldata _comment,
+        address _nftAddress,
+        uint256 _tokenId
     ) external onlyRole(COMMENTER_ROLE) {
         Coordinate memory coord = Coordinate({x: _x, y: _y});
 
@@ -69,15 +99,88 @@ contract TranscriptOfLuci is AccessControlUpgradeable, OwnableUpgradeable {
             coordinate: coord
         });
 
-        _addComment(_x, _y, comment);
+        _addCommentForNft(_x, _y, _nftAddress, _tokenId, comment);
     }
+
+    function batchAddComments(
+        uint256[] calldata _xs,
+        uint256[] calldata _ys,
+        address[] calldata _commenters,
+        string[] calldata _comments
+    ) external onlyRole(COMMENTER_ROLE) {
+        for (uint256 i = 0; i < _xs.length; ++i) {
+            Coordinate memory coord = Coordinate({x: _xs[i], y: _ys[i]});
+
+            Comment memory comment = Comment({
+                commenter: _commenters[i],
+                comment: _comments[i],
+                coordinate: coord
+            });
+
+            _addComment(_xs[i], _ys[i], comment);
+        }
+    }
+
+    function batchAddCommentsForNfts(
+        uint256[] calldata _xs,
+        uint256[] calldata _ys,
+        address[] calldata _commenters,
+        string[] calldata _comments,
+        address[] calldata _nftAddresses,
+        uint256[] calldata _tokenIds
+    ) external onlyRole(COMMENTER_ROLE) {
+        for (uint256 i = 0; i < _xs.length; ++i) {
+            Coordinate memory coord = Coordinate({x: _xs[i], y: _ys[i]});
+
+            Comment memory comment = Comment({
+                commenter: _commenters[i],
+                comment: _comments[i],
+                coordinate: coord
+            });
+
+            _addCommentForNft(_xs[i], _ys[i], _nftAddresses[i], _tokenIds[i], comment);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                            Get Comments Functions
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function getCommentForCoordinates(
+        uint256 _x,
+        uint256 _y
+    ) external view returns (Comment[] memory commentsForCoordinate) {
+        uint256[] storage indices = comments[_x][_y];
+        uint256 numComments = indices.length;
+
+        commentsForCoordinate = new Comment[](numComments);
+
+        for (uint256 i = 0; i < numComments; ++i) {
+            uint256 index = indices[i];
+            commentsForCoordinate[i] = rawComments[index];
+        }
+    }
+
+    function getCommentForNft(
+        address _nftContract,
+        uint256 _tokenId
+    ) external view returns (Comment memory) {
+        uint256 index = commentByNft[_nftContract][_tokenId];
+        return rawComments[index];
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                Internal Functions
+    //////////////////////////////////////////////////////////////////////////*/
 
     function _addComment(
         uint256 _x,
         uint256 _y,
         Comment memory _comment
     ) internal {
-        comments[_x][_y].push(_comment);
+        rawComments.push(_comment);
+        uint256 index = rawComments.length - 1;
+        comments[_x][_y].push(index);
     }
 
     function _addCommentForNft(
@@ -87,7 +190,10 @@ contract TranscriptOfLuci is AccessControlUpgradeable, OwnableUpgradeable {
         uint256 _tokenId,
         Comment memory _comment
     ) internal {
-        commentByNft[_nftContract][_tokenId] = _comment;
-        comments[_x][_y].push(_comment);
+        rawComments.push(_comment);
+        uint256 index = rawComments.length - 1;
+
+        commentByNft[_nftContract][_tokenId] = index;
+        comments[_x][_y].push(index);
     }
 }
---
+++ test/TranscriptOfLuci.t.sol
@@ -0,0 +1,261 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TranscriptOfLuci, Comment} from "../src/TranscriptOfLuci.sol";
+
+contract TranscriptOfLuciTest is Test {
+    TranscriptOfLuci public transcript;
+    address public alice;
+    address public bob;
+    address public charles;
+    address public nft;
+
+    function setUp() public {
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
+        charles = makeAddr("charles");
+        nft = makeAddr("nft");
+
+        transcript = new TranscriptOfLuci(false);
+        transcript.initialize(alice);
+
+        vm.startPrank(alice);
+        transcript.grantRole(transcript.COMMENTER_ROLE(), bob);
+        vm.stopPrank();
+    }
+
+    function test_addComment() public {
+        vm.prank(bob);
+        transcript.addComment(4720423717920495500000000, 3063794169465206600000000, charles, "Does anything truly exist beyond our own mind? We see only what our mind allows, the picture through the foggy lens of fear and hope. I see the best of me, building the temple of a better world, lending a hand to those that still cling to solid ground for fear to Wander the unknown. I see the worst of me, the masks I wear, the moments where I'm raw and cruel and push others away. And all the while she holds me at the edge, I feel her hand in mine, and I see not what I am, but the best that I can be.");
+
+        Comment[] memory comments = transcript.getCommentForCoordinates(4720423717920495500000000, 3063794169465206600000000);
+        assertEq(comments.length, 1, "comment length");
+
+        console.log(comments[0].commenter);
+        console.log(comments[0].comment);
+        console.log(comments[0].coordinate.x);
+        console.log(comments[0].coordinate.y);
+    }
+
+    function test_addCommentForNft() public {
+        vm.prank(bob);
+        transcript.addCommentForNft(420, 120, alice, "hello again, world!", nft, 1);
+
+        Comment memory comment = transcript.getCommentForNft(nft, 1);
+        console.log(comment.commenter);
+        console.log(comment.comment);
+        console.log(comment.coordinate.x);
+        console.log(comment.coordinate.y);
+    }
+
+    function test_addMultipleComments() public {
+        vm.startPrank(bob);
+        transcript.addComment(120, 420, charles, "hello, world!");
+        transcript.addComment(120, 420, alice, "im here too!");
+        vm.stopPrank();
+
+        Comment[] memory comments = transcript.getCommentForCoordinates(120, 420);
+        assertEq(comments.length, 2, "comment length");
+
+        console.log(comments[0].commenter);
+        console.log(comments[0].comment);
+        console.log(comments[0].coordinate.x);
+        console.log(comments[0].coordinate.y);
+        console.log();
+        console.log(comments[1].commenter);
+        console.log(comments[1].comment);
+        console.log(comments[1].coordinate.x);
+        console.log(comments[1].coordinate.y);
+    }
+
+    function test_batchAddCommentsSameCoordinate() public {
+        uint256[] memory xs = new uint256[](2);
+        xs[0] = 120;
+        xs[1] = 120;
+
+        uint256[] memory ys = new uint256[](2);
+        ys[0] = 420;
+        ys[1] = 420;
+
+        address[] memory commenters = new address[](2);
+        commenters[0] = charles;
+        commenters[1] = alice;
+
+        string[] memory messages = new string[](2);
+        messages[0] = "hello, world!";
+        messages[1] = "im here too!";
+
+        vm.startPrank(bob);
+        transcript.batchAddComments(xs, ys, commenters, messages);
+        vm.stopPrank();
+
+        Comment[] memory comments = transcript.getCommentForCoordinates(120, 420);
+        assertEq(comments.length, 2, "comment length");
+
+        console.log(comments[0].commenter);
+        console.log(comments[0].comment);
+        console.log(comments[0].coordinate.x);
+        console.log(comments[0].coordinate.y);
+        console.log();
+        console.log(comments[1].commenter);
+        console.log(comments[1].comment);
+        console.log(comments[1].coordinate.x);
+        console.log(comments[1].coordinate.y);
+    }
+
+    function test_batchAddCommentsDiffCoordinate() public {
+        uint256[] memory xs = new uint256[](2);
+        xs[0] = 120;
+        xs[1] = 420;
+
+        uint256[] memory ys = new uint256[](2);
+        ys[0] = 420;
+        ys[1] = 120;
+
+        address[] memory commenters = new address[](2);
+        commenters[0] = charles;
+        commenters[1] = alice;
+
+        string[] memory messages = new string[](2);
+        messages[0] = "hello, world!";
+        messages[1] = "im here too!";
+
+        vm.startPrank(bob);
+        transcript.batchAddComments(xs, ys, commenters, messages);
+        vm.stopPrank();
+
+        Comment[] memory comments = transcript.getCommentForCoordinates(120, 420);
+        assertEq(comments.length, 1, "comment length");
+
+        console.log(comments[0].commenter);
+        console.log(comments[0].comment);
+        console.log(comments[0].coordinate.x);
+        console.log(comments[0].coordinate.y);
+        console.log();
+
+        comments = transcript.getCommentForCoordinates(420, 120);
+        assertEq(comments.length, 1, "comment length");
+
+        console.log(comments[0].commenter);
+        console.log(comments[0].comment);
+        console.log(comments[0].coordinate.x);
+        console.log(comments[0].coordinate.y);
+    }
+
+    function test_batchAddCommentsForNftsSameCoordinate() public {
+        uint256[] memory xs = new uint256[](2);
+        xs[0] = 120;
+        xs[1] = 120;
+
+        uint256[] memory ys = new uint256[](2);
+        ys[0] = 420;
+        ys[1] = 420;
+
+        address[] memory commenters = new address[](2);
+        commenters[0] = charles;
+        commenters[1] = alice;
+
+        string[] memory messages = new string[](2);
+        messages[0] = "hello, world!";
+        messages[1] = "im here too!";
+
+        address[] memory nftContracts = new address[](2);
+        nftContracts[0] = nft;
+        nftContracts[1] = nft;
+
+        uint256[] memory tokenIds = new uint256[](2);
+        tokenIds[0] = 1;
+        tokenIds[1] = 2;
+
+        vm.startPrank(bob);
+        transcript.batchAddCommentsForNfts(xs, ys, commenters, messages, nftContracts, tokenIds);
+        vm.stopPrank();
+
+        Comment[] memory comments = transcript.getCommentForCoordinates(120, 420);
+        assertEq(comments.length, 2, "comment length");
+
+        console.log(comments[0].commenter);
+        console.log(comments[0].comment);
+        console.log(comments[0].coordinate.x);
+        console.log(comments[0].coordinate.y);
+        console.log();
+        console.log(comments[1].commenter);
+        console.log(comments[1].comment);
+        console.log(comments[1].coordinate.x);
+        console.log(comments[1].coordinate.y);
+        console.log();
+
+        Comment memory comment = transcript.getCommentForNft(nft, 1);
+        console.log(comment.commenter);
+        console.log(comment.comment);
+        console.log(comment.coordinate.x);
+        console.log(comment.coordinate.y);
+        console.log();
+        comment = transcript.getCommentForNft(nft, 2);
+        console.log(comment.commenter);
+        console.log(comment.comment);
+        console.log(comment.coordinate.x);
+        console.log(comment.coordinate.y);
+    }
+
+    function test_batchAddCommentsForNftsDiffCoordinate() public {
+        uint256[] memory xs = new uint256[](2);
+        xs[0] = 120;
+        xs[1] = 420;
+
+        uint256[] memory ys = new uint256[](2);
+        ys[0] = 420;
+        ys[1] = 120;
+
+        address[] memory commenters = new address[](2);
+        commenters[0] = charles;
+        commenters[1] = alice;
+
+        string[] memory messages = new string[](2);
+        messages[0] = "hello, world!";
+        messages[1] = "im here too!";
+
+       address[] memory nftContracts = new address[](2);
+        nftContracts[0] = nft;
+        nftContracts[1] = nft;
+
+        uint256[] memory tokenIds = new uint256[](2);
+        tokenIds[0] = 1;
+        tokenIds[1] = 2;
+
+        vm.startPrank(bob);
+        transcript.batchAddCommentsForNfts(xs, ys, commenters, messages, nftContracts, tokenIds);
+        vm.stopPrank();
+
+        Comment[] memory comments = transcript.getCommentForCoordinates(120, 420);
+        assertEq(comments.length, 1, "comment length");
+
+        console.log(comments[0].commenter);
+        console.log(comments[0].comment);
+        console.log(comments[0].coordinate.x);
+        console.log(comments[0].coordinate.y);
+        console.log();
+
+        comments = transcript.getCommentForCoordinates(420, 120);
+        assertEq(comments.length, 1, "comment length");
+
+        console.log(comments[0].commenter);
+        console.log(comments[0].comment);
+        console.log(comments[0].coordinate.x);
+        console.log(comments[0].coordinate.y);
+        console.log();
+
+        Comment memory comment = transcript.getCommentForNft(nft, 1);
+        console.log(comment.commenter);
+        console.log(comment.comment);
+        console.log(comment.coordinate.x);
+        console.log(comment.coordinate.y);
+        console.log();
+        comment = transcript.getCommentForNft(nft, 2);
+        console.log(comment.commenter);
+        console.log(comment.comment);
+        console.log(comment.coordinate.x);
+        console.log(comment.coordinate.y);
+    }
+}
